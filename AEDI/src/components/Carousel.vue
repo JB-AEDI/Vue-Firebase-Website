d5344ab2164a08ef95ebd47d9b2ffab1d5a82702
huketo <75968351+huketo@users.noreply.github.com>
2022-06-13 21:35:47 +0900

    Add Carousel, mainpage update

---
+++ AEDI/src/components/Carousel.vue
@@ -0,0 +1,60 @@
+<template>
+  <Carousel :itemsToShow="3.95" :wrapAround="true">
+    <Slide v-for="slide in 10" :key="slide">
+      <div class="carousel__item">{{ slide }}</div>
+    </Slide>
+
+    <template #addons>
+      <Navigation />
+      <Pagination />
+    </template>
+  </Carousel>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { Carousel, Pagination, Slide, Navigation } from "vue3-carousel";
+
+import "vue3-carousel/dist/carousel.css";
+
+export default defineComponent({
+  name: "Autoplay",
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+});
+</script>
+
+<style scoped>
+.carousel__item {
+  min-height: 300px;
+  width: 80%;
+  background-color: var(--vc-clr-primary);
+  color: var(--vc-clr-white);
+  font-size: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.carousel__slide > .carousel__item {
+  transform: scale(1);
+  opacity: 0.5;
+  transition: 0.5s;
+}
+.carousel__slide--visible > .carousel__item {
+  opacity: 1;
+  transform: rotateY(0);
+}
+.carousel__slide--next > .carousel__item {
+  transform: scale(0.9) translate(-10px);
+}
+.carousel__slide--prev > .carousel__item {
+  transform: scale(0.9) translate(10px);
+}
+.carousel__slide--active > .carousel__item {
+  transform: scale(1.1);
+}
+</style>
--- AEDI/src/pages/MainPage.vue
+++ AEDI/src/pages/MainPage.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>
-    <h1>Main Page</h1>
+  <div class="mx-5">
+    <Carousel></Carousel>
+    <div class="flex justify-center gap-10 mt-7">
+      <div class="bg-gray-200 py-3 px-4 flex items-center gap-3">
+        <div>
+          <i class="fa-solid fa-clock-rotate-left fa-2xl"></i>
+        </div>
+        <div class="flex flex-col items-center">
+          <div>150</div>
+          <div>마감임박</div>
+        </div>
+      </div>
+      <div class="bg-gray-200 py-3 px-4 flex items-center gap-3">
+        <div>
+          <i class="fa-solid fa-cloud-arrow-up fa-2xl"></i>
+        </div>
+        <div class="flex flex-col items-center">
+          <div>239</div>
+          <div>최근등록</div>
+        </div>
+      </div>
+      <div class="bg-gray-200 py-3 px-4 flex items-center gap-3">
+        <div>
+          <i class="fa-solid fa-bullhorn fa-2xl"></i>
+        </div>
+        <div class="flex flex-col items-center">
+          <div>342</div>
+          <div>진행중</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { useRouter } from "vue-router";
-
-const router = useRouter();
+import Carousel from "../components/Carousel.vue";
 </script>

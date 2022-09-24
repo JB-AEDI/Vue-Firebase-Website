7acee9b236ce2b3194e6b8c8b63578edbb93871a
huketo <75968351+huketo@users.noreply.github.com>
2022-09-24 20:55:22 +0900

    add - Footer

--- AEDI/src/App.vue
+++ AEDI/src/App.vue
@@ -3,12 +3,14 @@
   <div class="mt-6">
     <RouterView />
   </div>
+  <Footer></Footer>
 </template>
 
 <script setup>
 import Header from "./components/Header.vue";
 import { user, getUserProfile } from "./firebase/user";
 import { ref, watch, provide } from "vue";
+import Footer from "./components/Footer.vue";
 
 const userProfile = ref({
   name: "",
---
+++ AEDI/src/components/Footer.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="my-16 flex justify-center">footer 입니다.</div>
+</template>
